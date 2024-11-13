02d288cdc335574655bfc715d29b66b03399a73a
Sourabh Jarad <156126729+sourabh190304@users.noreply.github.com>
2024-11-13 21:23:29 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,52 @@
+//1.ways to print in java script 
+
+console.log("hello world");
+alert("me");
+document.write("this is document write");
+
+
+//2.console api java script
+
+console.log("hello world", 4 + 5, "another log");
+console.warn("this is my warnng");
+console.error("by mistake error");
+console.assert(4 == 6);
+
+//3.javascript varibles
+
+//what sre varibles == container to store data values
+
+var number1 = 34;
+var number2 = 56;
+console.log(number1+number2);
+
+//4. data types in javascript
+
+var num1=455;
+var num2=56.87;
+
+console.log(num1);
+console.log(num2);
+
+var str1="this is string";
+var str2='this is also a string';
+
+console.log(str1,str2);
+
+//objects
+var marks ={
+    ravi:34,
+    kiran:87,
+    harry:99.99
+}
+console.log(marks)
+
+//boolean
+
+var a= true;
+var b= false;
+
+console.log(a);
+console.log(b);
+
+
